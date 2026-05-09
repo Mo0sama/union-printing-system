ca3d0710616b00fc38c9b397ba104f22dcccdbd3
Mo0sama <mossama@pythonanywhere.com>
2026-05-09 14:37:16 +0300

    Fix active_overrides to pass count instead of QuerySet in backoffice dashboard

--- backend/apps/backoffice/views.py
+++ backend/apps/backoffice/views.py
@@ -16,7 +16,7 @@ def superuser_required(view):
 def dashboard(request):
     total_labels = SystemLabel.objects.exclude(app_label__in=EXCLUDED_APPS).count()
     customized = SystemLabel.objects.filter(is_active=True).exclude(app_label__in=EXCLUDED_APPS).exclude(value_ar='').exclude(value_ar=F('default_value')).count()
-    active_overrides = SystemLabel.objects.filter(is_active=True).exclude(app_label__in=EXCLUDED_APPS).exclude(value_ar='').exclude(value_ar=F('default_value'))
+    active_overrides = SystemLabel.objects.filter(is_active=True).exclude(app_label__in=EXCLUDED_APPS).exclude(value_ar='').exclude(value_ar=F('default_value')).count()
     apps_list = (
         SystemLabel.objects.exclude(app_label__in=EXCLUDED_APPS)
         .values('app_label')
